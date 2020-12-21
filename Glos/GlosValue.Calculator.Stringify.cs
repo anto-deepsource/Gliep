c1ced1bd2885a5aa451326227aebfb91d9247d09
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2020-12-21 20:13:37 +0800

    add debug stringifying to new external functions

--- Glos/GlosValue.Calculator.Stringify.cs
+++ Glos/GlosValue.Calculator.Stringify.cs
@@ -10,16 +10,18 @@ namespace GeminiLab.Glos {
             [ExcludeFromCodeCoverage]
             public static string DebugStringify(in GlosValue v) {
                 return v.Type switch {
-                    GlosValueType.Nil              => "nil",
-                    GlosValueType.Integer          => v.AssumeInteger().ToString(),
-                    GlosValueType.Float            => v.AssumeFloat().ToString(CultureInfo.InvariantCulture),
-                    GlosValueType.Boolean          => v.AssumeBoolean() ? "true" : "false",
-                    GlosValueType.Table            => $"<table: {RuntimeHelpers.GetHashCode(v.AssumeTable()):x8}>",
-                    GlosValueType.String           => $"\"{EscapeSequenceConverter.Encode(v.AssumeString())}\"",
-                    GlosValueType.Function         => $"<function \"{v.AssumeFunction().Prototype.Name} {(v.AssumeFunction().ParentContext is {} ctx ? $"at {RuntimeHelpers.GetHashCode(ctx):x8}" : "unbound")}\">",
-                    GlosValueType.ExternalFunction => $"<external function: {RuntimeHelpers.GetHashCode(v.AssumeExternalFunction()):x8}>",
-                    GlosValueType.Vector           => $"<vector: {RuntimeHelpers.GetHashCode(v.AssumeVector()):x8}>",
-                    _                              => throw new ArgumentOutOfRangeException()
+                    GlosValueType.Nil                   => "nil",
+                    GlosValueType.Integer               => v.AssumeInteger().ToString(),
+                    GlosValueType.Float                 => v.AssumeFloat().ToString(CultureInfo.InvariantCulture),
+                    GlosValueType.Boolean               => v.AssumeBoolean() ? "true" : "false",
+                    GlosValueType.Table                 => $"<table: {RuntimeHelpers.GetHashCode(v.AssumeTable()):x8}>",
+                    GlosValueType.String                => $"\"{EscapeSequenceConverter.Encode(v.AssumeString())}\"",
+                    GlosValueType.Function              => $"<function \"{v.AssumeFunction().Prototype.Name} {(v.AssumeFunction().ParentContext is {} ctx ? $"at {RuntimeHelpers.GetHashCode(ctx):x8}" : "unbound")}\">",
+                    GlosValueType.ExternalFunction      => $"<external function: {RuntimeHelpers.GetHashCode(v.AssumeExternalFunction()):x8}>",
+                    GlosValueType.Vector                => $"<vector: {RuntimeHelpers.GetHashCode(v.AssumeVector()):x8}>",
+                    GlosValueType.ExternalPureFunction  => $"<external pure function: {RuntimeHelpers.GetHashCode(v.AssumeExternalPureFunction()):x8}>",
+                    GlosValueType.ExternalAsyncFunction => $"<external async function: {RuntimeHelpers.GetHashCode(v.AssumeExternalAsyncFunction()):x8}>",
+                    _                                   => throw new ArgumentOutOfRangeException()
                 };
             }
         }
